1646c19de5ef47ea97c82452cba584096bf32a06
Romeus <romeusmeister@gmail.com>
2015-02-27 16:49:19 +0300

    Pass to the access denied template full url string instead of domain string

--- whitelist.py
+++ whitelist.py
@@ -14,10 +14,7 @@ with open ("access_denied_template.html", "r") as myfile:
     template=myfile.read()
 
 def request(context, flow):
-
-#    with open ("access_denied_template.html", "r") as myfile:
-#        template=myfile.read()
-
+    
     # pretty_host(hostheader=True) takes the Host: header of the request into account,
     # which is useful in transparent mode where we usually only have the IP otherwise.
 
@@ -27,5 +24,5 @@ def request(context, flow):
         resp = HTTPResponse(
             [1, 1], 200, "OK",
             ODictCaseless([["Content-Type", "text/html"]]),
-            template.replace('$URL', flow.request.pretty_host(hostheader=True)))
+            template.replace('$URL', flow.request.url))
         flow.reply(resp)
